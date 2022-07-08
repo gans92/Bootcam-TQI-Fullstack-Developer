d2c26b97a2db3beb167d17b0e156c548ee8f32f4
gans92 <gabri.alisson.23@gmail.com>
2022-07-08 16:31:28 -0500

    projeto-final-java

---
+++ java/Resumo-aulas.java
@@ -0,0 +1,52 @@
+// print(hello world)
+
+// int quantidadeProduto = camelcase
+// constante = 
+// int final NUMERO_TENTAIVAS = 5 = TUDO MAISCULO
+
+public class Teste {
+    public static void main(String[] args) {
+
+        final int NUMERO_TENTAIVAS = 5;
+        System.out.println(NUMERO_TENTAIVAS);
+    }
+
+// operadores aritmeticos
+// pos fixado: exp++ ou exp--
+// pre fixado: ++exp ou --exp
+// aritmeticos: + - * / %
+// atribuição: = += -= *= /= %=
+
+// ex:
+// int x = ++k > i = k +1
+// i += 5 > i = i + 5
+
+// byte
+// short
+// char
+// int
+// long
+// float
+// double
+
+// int i;
+// long l = 100;
+// i = (int) l;
+
+// public,protected,private
+// static, final
+
+public String getNome() {
+    return getNome();
+    // String nome;
+    // return nome;
+};
+}
+
+// public static tipodedado nomedometodo(parametro){corpo}
+
+// chamar o metodo pela classe / objeto = Math.random() / usuario.getEmail()
+// sempre a letra da classe é maiscula e do obejto minuscula
+
+// assinatura = nome + parametro 
+
